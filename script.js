8d8d47fe64b93eacefd70060b32d4500d8e33b86
Jaro233 <jaroslaw.waliczek@vp.pl>
2022-12-03 17:36:53 +0100

    rework

--- script.js
+++ script.js
@@ -3,9 +3,14 @@ const openButton = document.querySelector(".openBtn");
 const closeButton = document.querySelector(".closeBtn");
 
 function openNav() {
-    overlay.classList.remove("hidden")
+    overlay.classList.remove("invisible")
+    overlay.classList.remove("w-0")
+    overlay.classList.add("w-full")
 }
   
 function closeNav() {
-    overlay.classList.add("hidden")
+    overlay.classList.add("invisible")
+    overlay.classList.add("w-0")
+    overlay.classList.remove("w-full")
 }
+
